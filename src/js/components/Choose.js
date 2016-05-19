67a6e27a2885b15386edbff353ef6501f8dadb8e
Marie-Eve Gauthier <gauthier.marie.eve@gmail.com>
2016-05-19 23:05:55 +0000

    passing choose pizza data between components

--- src/js/components/Choose.js
+++ src/js/components/Choose.js
@@ -8,32 +8,32 @@ var pizzas = [{
   photo: '../image/pizzas/tomato-mozzarella-egg.jpg',
   name: 'Egg',
   cheese: 'mozzarella',
-  toppings: ['sauce', 'tomatoes', 'egg' ],
+  toppings: ['sauce', 'tomatoes', 'egg'],
   price: 7
 }, {
-  
+
   photo: '../image/pizzas/tomato-cheddar-broccoli.jpg',
   name: 'Broccoli',
   cheese: 'cheddar',
-  toppings: ['sauce', 'tomatoes', 'broccolis' ],
+  toppings: ['sauce', 'tomatoes', 'broccolis'],
   price: 7
 }, {
   photo: '../image/pizzas/spicy-sausage.jpg',
   name: 'Spicy-sausage',
   cheese: 'mozzarella',
-  toppings: ['sauce', 'spicy sausages' ],
+  toppings: ['sauce', 'spicy sausages'],
   price: 10
 }, {
   photo: '../image/pizzas/chicken-mozzarella-avocado.jpg',
   name: 'Chicken-avocado',
   cheese: 'mozzarella',
-  toppings: ['sauce', 'chicken', 'avocados' ],
+  toppings: ['sauce', 'chicken', 'avocados'],
   price: 11
 }, {
   photo: '../image/pizzas/bakedPotato-bacon-cheddar-sourCream-chives.jpg',
   name: 'Potato-bacon',
   cheese: 'cheddar',
-  toppings: ['sour cream', 'baked potatoes', 'bacon', 'chives' ],
+  toppings: ['sour cream', 'baked potatoes', 'bacon', 'chives'],
   price: 12
 }];
 
@@ -43,24 +43,58 @@ var Choose = React.createClass({
   getInitialState: function() {
     var choice = data.getData('choice') || {};
     return {
-      selectedPizzas: [],
-      name: choice.name || ''
+      selectedPizzas: []
     };
   },
-  
+
   handleChoice: function(ev) {
     var that = this;
-    var PizzaChosen = {
+    var pizzaChosen = {
       name: ev.target.name,
       checked: ev.target.checked,
       value: ev.target.value
     };
-    this.setState({
-      selectedPizzas: that.state.selectedPizzas.concat([PizzaChosen])
-    });
+
+
+    if (this.state.selectedPizzas.length === 0) {
+      data.setData('pizza', that.state.selectedPizzas.concat([pizzaChosen]))
+      this.setState({
+        selectedPizzas: this.state.selectedPizzas.concat([pizzaChosen])
+      });
+
+    }
+    else {
+
+      var index = this.state.selectedPizzas.findIndex(function(i) {
+
+        if (i.name === pizzaChosen.name) {
+          return i
+        }
+      })
+
+      var oldState = this.state.selectedPizzas;
+      if(index !== -1){
+         
+         oldState.splice(index, 1)
+         data.setData('pizza', oldState)
+         this.setState({
+           selectedPizzas: oldState
+         })
+      } else {
+        
+        data.setData('pizza', that.state.selectedPizzas.concat([pizzaChosen]))
+        this.setState({
+          selectedPizzas: this.state.selectedPizzas.concat([pizzaChosen])
+        })
+      }
+
+    }
   },
-  
+
   render: function() {
+console.log(this.state.selectedPizzas, "HERE IS PIZZA")
+
+
     var that = this;
     var lis = pizzas.map(function(pizza) {
       return (
--- src/js/data.js
+++ src/js/data.js
@@ -1,13 +1,22 @@
 var data = {};
 
+{
+    mazarele: 
+}
+
 function getData(key) {
     return data[key];
 }
 
 function setData(key, value) {
     data[key] = value;
+    //window.localStorage.clear();
+    window.localStorage.setItem(key, JSON.stringify(value));
+}
+
+function removeData(key) {
+    window.localStorage.removeItem(key);
     
-    localStorage.setItem(key, JSON.stringify(value));
 }
 
 module.exports = {
